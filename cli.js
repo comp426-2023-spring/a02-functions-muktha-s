8cb4ec231b799a79d1501518f17c6e94099e576e
Muktha Santhoshkumar <muktha@email.unc.edu>
2023-02-06 15:15:06 -0500

    const response

--- cli.js
+++ cli.js
@@ -57,7 +57,10 @@ if (args.w) {
 if (!(long && lat)) {
 	console.log("Latitude must be in range"); 
 	process.exit(0); 
-} 
+}
+
+const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + lat
++ "&longitude=" + long + "&timezone=" + timezone + "&daily=precipitation_hours"); 
 	
 	
 
